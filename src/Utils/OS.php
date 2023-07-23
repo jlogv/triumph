03ee9f0bf64a30ed2036af17cabd81ee2d10ce3e
Logvinov Alexey <>
2023-07-23 15:15:39 +0300

    add some utils & request class

---
+++ src/Utils/OS.php
@@ -0,0 +1,57 @@
+<?php
+
+    /**
+     * Project: Triumph Framework
+     * Class: OS
+     * Copyright (c) Logvinov Alexey, 2014-2023. All rights reserved.
+     */
+
+    namespace Triumph\Utils;
+
+    class OS {
+
+        /**
+         * Windows
+         */
+        public const OS_WIN = 1;
+
+        /**
+         * Linux, Unix
+         */
+        public const OS_NIX = 2;
+
+        /**
+         * Macintosh
+         */
+        public const OS_MAC = 3;
+
+        /**
+         * Check is Windows operating system
+         * @return bool
+         */
+        public static function isWindows() : bool
+        {
+            return DIRECTORY_SEPARATOR == '\\';
+        }
+
+        /**
+         * Check is nix operating system
+         * @return bool
+         */
+        public static function isLinux() : bool
+        {
+            return DIRECTORY_SEPARATOR != '\\';
+        }
+
+        /**
+         * Get current type of operating system
+         * @return int
+         */
+        public static function getOS() : int
+        {
+            $result = 0;
+            if (self::isWindows()) return self::OS_WIN;
+            if (self::isLinux()) return self::OS_NIX;
+            return $result;
+        }
+    }
---
+++ src/Utils/Strings.php
@@ -0,0 +1,71 @@
+<?php
+
+    /**
+     * Project: Triumph Framework
+     * Class: Strings
+     * Copyright (c) Logvinov Alexey, 2014-2023. All rights reserved.
+     */
+
+    namespace Triumph\Utils;
+
+    use Triumph\Http\Request;
+
+    class Strings
+    {
+        /**
+         * Multibyte string to lower
+         *
+         * @param string $string
+         * @return string
+         */
+        public static function lower(string $string) : string
+        {
+            return mb_strtolower($string);
+        }
+
+        /**
+         * Multibyte string to upper
+         *
+         * @param string $string
+         * @return string
+         */
+        public static function upper(string $string) : string
+        {
+            return mb_strtoupper($string);
+        }
+
+        /**
+         * Check is valid JSON string
+         *
+         * @param string $string
+         * @return bool
+         */
+        public static function isJson(string $string) : bool
+        {
+            json_decode($string);
+            return (json_last_error() == JSON_ERROR_NONE);
+        }
+
+        /**
+         * Get multibyte string length
+         *
+         * @param string $value string
+         * @return int length
+         */
+        public static function length(string $value): int
+        {
+            return mb_strlen($value);
+        }
+
+
+        /**
+         * Delete extra slashes then magic methods used
+         *
+         * @param mixed $data used data
+         * @return array|string clear data
+         */
+        public static function strip_slashes(mixed $data): array|string
+        {
+            return is_array($data) ? array_map(array('self::strip_slashes'), $data) : stripslashes($data);
+        }
+    }
