f59097ccaa32ccbdeb88037fa8a09878cec7acc9
Alexey Logvinov <jlogvinov@gmail.com>
2023-07-28 03:19:19 +0300

    add base exception class

---
+++ src/Kernel/Exception.php
@@ -0,0 +1,14 @@
+<?php
+
+    /**
+     * Project: Triumph Framework
+     * Class: Exception
+     * Copyright (c) Alexey Logvinov, 2014-2023. All rights reserved.
+     */
+
+    namespace Triumph\Kernel;
+
+    class Exception extends \Exception
+    {
+
+    }
